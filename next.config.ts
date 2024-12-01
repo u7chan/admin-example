9cade040286dc04931f0b49c725d12230279de07
u7chan <34462401+u7chan@users.noreply.github.com>
2024-12-02 00:10:24 +0900

    fix: server actions config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: process.env.BASE_PATH,
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:8080", // for reverse proxy
+      ],
+    },
+  },
 };
 
 export default nextConfig;
