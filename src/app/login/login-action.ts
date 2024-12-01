0f48efc7f13288dc9db8dbcab2e110e8ec116e2c
u7chan <34462401+u7chan@users.noreply.github.com>
2024-12-02 00:24:44 +0900

    feat: login auth

--- src/app/login/login-action.ts
+++ src/app/login/login-action.ts
@@ -1,5 +1,9 @@
 "use server";
 
 export async function loginAction(formData: FormData) {
-  console.log(formData);
+  const email = formData.get("email");
+  const password = formData.get("password");
+  if (email !== "example@example.com" || password !== "test") {
+    throw new Error("Unauthorized");
+  }
 }
