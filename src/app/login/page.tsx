a2b8e27df9975abf6f3f67b94c9c402d3fcdcdc8
u7chan <34462401+u7chan@users.noreply.github.com>
2024-12-01 23:46:29 +0900

    feat: login layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -8,8 +12,37 @@ export default async function Page() {
     redirect("/");
   }
   return (
-    <>
-      <p>Login</p>
-    </>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <Card className="max-w-md w-full">
+        <CardHeader>
+          <h2 className="text-center text-2xl font-bold">ログイン</h2>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <div>
+              <Label htmlFor="email">メールアドレス</Label>
+              <Input
+                type="email"
+                id="email"
+                required
+                placeholder="example@example.com"
+              />
+            </div>
+            <div>
+              <Label htmlFor="password">パスワード</Label>
+              <Input
+                type="password"
+                id="password"
+                required
+                placeholder="********"
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              ログイン
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
