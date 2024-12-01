7b734256aa67fc9f2c788359ff13f44f71cf6efe
u7chan <34462401+u7chan@users.noreply.github.com>
2024-12-01 23:07:23 +0900

    feat: reverse proxy for nginx

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+const BASE_PATH = process.env.BASE_PATH;
+
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       <Image
         aria-hidden
-        src="/globe.svg"
+        src={`${BASE_PATH}/globe.svg`}
         alt="Globe icon"
         width={16}
         height={16}
