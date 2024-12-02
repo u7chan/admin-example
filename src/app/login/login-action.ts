f8b6ab8d5da185b564ba193ac261b98e91935ad5
u7chan <34462401+u7chan@users.noreply.github.com>
2024-12-03 00:46:47 +0900

    feat: login action

--- src/app/login/login-action.ts
+++ src/app/login/login-action.ts
@@ -9,28 +9,40 @@ export interface FormState {
 
 export async function loginAction(
   _prevState: FormState,
-  formData: FormData,
+  formData: FormData
 ): Promise<FormState> {
+  let errorMessage = "";
+
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
-  let errorMessage = "";
-  await new Promise<void>((resolve) => setTimeout(() => resolve(), 1000));
-  if (email !== "example@example.com" || password !== "test") {
-    errorMessage = "認証に失敗しました。";
+
+  try {
+    await login({ email, password });
+
+    const cookieStore = await cookies();
+    cookieStore.set({
+      name: "session",
+      value: "dummy",
+      path: process.env.BASE_PATH,
+      sameSite: "strict",
+      secure: false,
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
+    });
+  } catch (e: unknown) {
+    errorMessage = e instanceof Error ? e.message : "";
   }
-  const cookieStore = await cookies();
-  cookieStore.set({
-    name: "session",
-    value: "dummy",
-    path: process.env.BASE_PATH,
-    sameSite: "strict",
-    secure: false,
-    httpOnly: true,
-    expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
-  });
   return {
     email,
     password,
     errorMessage,
   };
+}
+
+async function login(params: { email: string; password: string }) {
+  // TODO: fake authentication
+  await new Promise<void>((resolve) => setTimeout(() => resolve(), 1000));
+  if (params.email !== "example@example.com" || params.password !== "test") {
+    throw new Error("認証に失敗しました。");
+  }
 }
