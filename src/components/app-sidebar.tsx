0f660dd0458b5895d91a941f0a98577858dfda8e
u7chan <34462401+u7chan@users.noreply.github.com>
2024-12-05 00:15:36 +0900

    fix: Sidebar menu

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -16,130 +16,33 @@ import {
 
 // This is sample data.
 const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
   navMain: [
     {
-      title: "Getting Started",
+      title: "Main Menu",
       url: "#",
       items: [
         {
-          title: "Installation",
+          title: "My GPT",
           url: "#",
         },
         {
-          title: "Project Structure",
+          title: "Users",
           url: "#",
         },
       ],
     },
     {
-      title: "Building Your Application",
+      title: "Settings",
       url: "#",
       items: [
         {
-          title: "Routing",
+          title: "Your Profile",
           url: "#",
         },
         {
-          title: "Data Fetching",
+          title: "Logout",
           url: "#",
           isActive: true,
-        },
-        {
-          title: "Rendering",
-          url: "#",
-        },
-        {
-          title: "Caching",
-          url: "#",
-        },
-        {
-          title: "Styling",
-          url: "#",
-        },
-        {
-          title: "Optimizing",
-          url: "#",
-        },
-        {
-          title: "Configuring",
-          url: "#",
-        },
-        {
-          title: "Testing",
-          url: "#",
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
-          url: "#",
-        },
-        {
-          title: "Upgrading",
-          url: "#",
-        },
-        {
-          title: "Examples",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "API Reference",
-      url: "#",
-      items: [
-        {
-          title: "Components",
-          url: "#",
-        },
-        {
-          title: "File Conventions",
-          url: "#",
-        },
-        {
-          title: "Functions",
-          url: "#",
-        },
-        {
-          title: "next.config.js Options",
-          url: "#",
-        },
-        {
-          title: "CLI",
-          url: "#",
-        },
-        {
-          title: "Edge Runtime",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Architecture",
-      url: "#",
-      items: [
-        {
-          title: "Accessibility",
-          url: "#",
-        },
-        {
-          title: "Fast Refresh",
-          url: "#",
-        },
-        {
-          title: "Next.js Compiler",
-          url: "#",
-        },
-        {
-          title: "Supported Browsers",
-          url: "#",
-        },
-        {
-          title: "Turbopack",
-          url: "#",
         },
       ],
     },
